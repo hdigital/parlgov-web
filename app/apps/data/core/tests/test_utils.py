5dcf6f03bb5b900f09b1a9947148bccc663133ec
hdigital <doering@uni-bremen.de>
2026-09-19 18:57:45 +0000

    Parametrize 'clean_date_format' test

--- app/apps/data/core/tests/test_utils.py
+++ app/apps/data/core/tests/test_utils.py
@@ -29,11 +29,17 @@ def test_prev_next_object(db, django_user_model):
     assert previous_next_object(object_b)[0] == object_a
 
 
-def test_clean_date():
-    assert clean_date_format(None) == ""
-    assert clean_date_format(date(2020, 7, 1)) == "2020"
-    assert clean_date_format(date(2020, 2, 1)) == "2020-02"
-    assert clean_date_format(date(2020, 2, 2)) == "2020-02-02"
+@pytest.mark.parametrize(
+    "date_value, expected",
+    [
+        (None, ""),
+        (date(2020, 7, 1), "2020"),
+        (date(2020, 2, 1), "2020-02"),
+        (date(2020, 2, 2), "2020-02-02"),
+    ],
+)
+def test_clean_date(date_value, expected):
+    assert clean_date_format(date_value) == expected
 
 
 def test_get_country_or_404(db, country):
